36d6b1af791297b52285158c0f3c42d6ffd15a2b
rfaldetta <ross.faldetta@gmail.com>
2015-07-29 20:36:12 -0500

    Completes photogram golden seven HW

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,50 @@
 class PhotosController < ApplicationController
+
+#CREATE
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params["the_source"]
+    p.caption = params["the_caption"]
+    p.save
+
+    redirect_to("/photos")
+  end
+
+
+# READ
   def index
     @list_of_photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params["id"])
+  end
+
+
+# UPDATE
+  def edit_form
+    @photo = Photo.find(params["id"])
+  end
+
+  def update_row
+    p = Photo.find(params["id"])
+    p.source = params["the_source"]
+    p.caption = params["the_caption"]
+    p.save
+
+    redirect_to("/photos/" + params["id"].to_s)
+  end
+
+
+# DELETE
+  def destroy
+    p = Photo.find(params["id"])
+    p.destroy
+
+    redirect_to("/photos")
   end
 
 
